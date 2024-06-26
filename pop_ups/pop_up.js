71f9f0615d9df38463b5f9a92653f845e697c380
Allan <allanjosueguzmanjarquin@gmail.com>
2024-06-25 20:26:12 -0600

    ya casi

--- pop_ups/pop_up.js
+++ pop_ups/pop_up.js
@@ -1,5 +1,5 @@
-let close_button = document.getElementById('close-button');
-close_button.addEventListener("click", function(e) {
-    e.preventDefault();
-    document.getElementById("window-notice").style.display = "none";
+let close_button = document.getElementById("close-button");
+close_button.addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("window-notice").style.display = "none";
 });
